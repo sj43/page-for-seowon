31a8765be2fcc8ab23420b771b213bed1126f5ac
Seung Hun Jang <sj43@rice.edu>
2021-01-04 17:54:24 +0900

    added snow background

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Snowfall from "react-snowfall";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 
@@ -21,6 +22,7 @@ ReactDOM.render(
       <Route path="/login-page" component={LoginPage} />
       <Route path="/" component={Components} />
     </Switch>
+    <Snowfall />
   </Router>,
   document.getElementById("root")
 );
